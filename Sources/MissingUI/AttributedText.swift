8dd5399d25b60959ee3033340dc471d1ba649e6c
Tiziano Coroneo <tiziano@ticketswap.com>
2019-10-27 19:29:35 +0100

    Added attributed text

---
+++ Sources/MissingUI/AttributedText.swift
@@ -0,0 +1,50 @@
+//
+//  AttributedText.swift
+//  
+//
+//  Created by Tiziano Coroneo on 27/10/2019.
+//
+
+import Foundation
+import SwiftUI
+
+public struct AttributedText: UIViewRepresentable {
+
+    class LabelView : UIView {
+        private var label = UILabel()
+
+        var attributedText: NSAttributedString? {
+            get { label.attributedText }
+            set { label.attributedText = newValue }
+        }
+
+        init() {
+            super.init(frame: .zero)
+
+            self.addSubview(label)
+
+            label.numberOfLines = 0
+            label.frame = self.bounds
+            label.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        }
+
+        required init?(coder: NSCoder) {
+            super.init(coder: coder)
+        }
+    }
+
+    public let attributedString: NSAttributedString
+
+    public func makeUIView(
+        context: Context
+    ) -> LabelView {
+        return LabelView()
+    }
+
+    public func updateUIView(
+        _ uiView: LabelView,
+        context: UIViewRepresentableContext<AttributedText>
+    ) {
+        uiView.attributedText = attributedString
+    }
+}
