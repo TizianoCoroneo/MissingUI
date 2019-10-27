2aa823e79bc6437420da2cc5d03c131c00e387e3
Tiziano Coroneo <tiziano@ticketswap.com>
2019-10-27 17:52:54 +0100

    Added configuration to the buttonSheet

--- Sources/MissingUI/SheetButton.swift
+++ Sources/MissingUI/SheetButton.swift
@@ -8,28 +8,43 @@
 import Foundation
 import SwiftUI
 
-public struct SheetButton<Destination: View>: View {
+public struct SheetButton<Configured: View, Destination: View>: View {
 
     @State private var isPresented: Bool = false
 
     private let title: String
+    private let configuration: (Button<Text>) -> Configured
     private let destination: (Binding<Bool>) -> Destination
 
     public init(
         title: String,
+        @ViewBuilder configuration: @escaping (Button<Text>) -> Configured,
         @ViewBuilder destination: @escaping (Binding<Bool>) -> Destination
     ) {
         self.title = title
+        self.configuration = configuration
         self.destination = destination
     }
 
     public var body: some View {
-        Button(
+        self.configuration(Button.init(
             title,
             action: {
                 self.isPresented = true
-        }).sheet(isPresented: self.$isPresented) {
+        })).sheet(isPresented: self.$isPresented) {
             self.destination(self.$isPresented)
         }
+    }
+}
+
+extension SheetButton where Configured == Button<Text> {
+    public init(
+        title: String,
+        @ViewBuilder destination: @escaping (Binding<Bool>) -> Destination
+    ) {
+        self.init(
+            title: title,
+            configuration: { $0 },
+            destination: destination)
     }
 }
