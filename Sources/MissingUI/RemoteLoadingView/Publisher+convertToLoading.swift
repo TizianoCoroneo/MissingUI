005f0c4b5b7583505609f076bbb4bca6e5dd5e08
Tiziano Coroneo <tiziano@ticketswap.com>
2019-12-01 18:23:22 +0100

    Now the  modifier does not prepend automatically a loading state to the resulting publisher.

--- Sources/MissingUI/RemoteLoadingView/Publisher+convertToLoading.swift
+++ Sources/MissingUI/RemoteLoadingView/Publisher+convertToLoading.swift
@@ -13,14 +13,12 @@ import Combine
 public extension Publisher {
     /**
      Converts a `Publisher<Output, Failure>` in a `Publisher<Loading<Output, Failure>, Never>`.
-     It prepends a `.loading` value to the input publisher, to represent the initial state.
 
      This allows you to use a failable `Publisher` with the `.assign` `Subscriber` that does not allow failable initializers.
      Once you have the result of this function, you can use it together with a `RemoteLoadingView` to display a certain `View` for the `.success` case, a different one for the `.failure` case and another different `View` for the `.loading` case.
      */
     func convertToLoading() -> AnyPublisher<Loading<Self.Output, Self.Failure>, Never> {
         map { .success($0) }
-            .prepend(Loading.loading)
             .catch { Just(.failure($0)) }
             .eraseToAnyPublisher()
     }
